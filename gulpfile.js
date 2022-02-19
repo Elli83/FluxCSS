793cc0076c6f26289ba6e0c6709f64eef0179921
Elliot <elliothills42@gmail.com>
2022-02-19 17:59:43 +0000

    sorted folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-    return src('scss/index.scss')
-    .pipe(sass())
-    .pipe(dest('css'))
+    return src('flux/**/*.scss')
+        .pipe(sass())
+        .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['scss/index.scss'], buildStyles)
+    watch(['flux/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
